fedc9163593fa2873b2c130245df1dd04212f9ce
paul <576330572@qq.com>
2022-12-11 22:17:49 +0800

    color cube

--- main.cpp
+++ main.cpp
@@ -5,15 +5,53 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <glm\glm.hpp>
+#include <glm\gtc\type_ptr.hpp> // glm::value_ptr
+#include <glm\gtc\matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include "Util.h"
 
 //#pragma comment(lib, "glew32.lib")  // 指定使用静态库，但是把dll放到产物文件夹里去掉了也能用
 
 using namespace std;
 
 #define numVAOs 1
+#define numVBOs 2
+
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
 
 GLuint renderingProgram;
 GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat;
+
+// 36个顶点，12个三角形，组成了纺织在原点出的2*2*2立方体
+void setupVertices() {
+	float vertexPositions[108] = {
+		-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,
+		-1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
+	};
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+}
 
 string readShaderSource(const char *filePath) {
 	string content;
@@ -52,15 +90,45 @@ GLuint createShaderProgram() {
 }
 
 void init(GLFWwindow *window) {
-	renderingProgram = createShaderProgram();
-	glGenVertexArrays(numVAOs, vao);
-	glBindVertexArray(vao[0]);
+	renderingProgram = Utils::createShaderProgram("shader/vShader.glsl", "shader/fShader.glsl");
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+	cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+	setupVertices();
 }
 
 void display(GLFWwindow *window, double currenTime) {
+	glClear(GL_DEPTH_BUFFER_BIT);
 	glUseProgram(renderingProgram);
-	glPointSize(30.0f);
-	glDrawArrays(GL_POINTS, 0, 1);
+
+	// 获取MV矩阵和投影矩阵的统一变量
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+
+	// 构建透视矩阵
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degress
+
+	// 构建视图矩阵、模型矩阵和视图-模型矩阵
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+
+	mvMat = vMat * mMat;
+
+	// 将透视矩阵和mv矩阵复制给相应的统一变量
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	// 将VBO关联给顶点着色器中相应的顶点属性
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	// https://docs.gl/gl4/glEnableVertexAttribArray
+	glEnableVertexAttribArray(0);
+
+	// 调整OpenGL设置，绘制模型
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 }
 
 void printShaderLog(GLuint shader) {
--- shader/fshader.glsl
+++ shader/fshader.glsl
@@ -1,5 +1,12 @@
 #version 430
+
 out vec4 color;
+
+in vec4 varyingColor;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+
 void main(void) {
-    color = vec4(0.0, 0.0, 1.0, 1.0);
+    color = varyingColor;
 }
